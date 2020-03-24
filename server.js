51a749de71400ac7e7ada184f300291a297e0583
Abdullah Yusefi <a.yusefi1991@gmail.com>
2020-03-24 18:21:10 +0300

    Modbus and Mysql Connection set up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "abd786",
+    password: "Memak123",
     database: "mydb"
   });
 
